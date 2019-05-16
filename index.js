a1435ceda560d8368d0f3b63d5fe44d6498fe617
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-05-16 13:11:25 +0100

    Set up and configure static folder.

--- index.js
+++ index.js
@@ -7,17 +7,24 @@ const path = require('path');
 //Initialise express
 const app = express();
 
+//Set and configure  Static folder
+
+app.use(express.static(path.join(__dirname, "client")));
+
 //Inject body-parser middleware
 app.use(bodyParser.json());
 
-//Generate a set of Vapid Keys for encryption: Private and Public.
+
+
+
+//Generate a set of Vapid Keys for encryption: Private and public.
 //cd into the middleware in node_modules and run command "generate-vapid-keys". See screenshot.
 //Store Vapid keys in variables, and as environmental variables before deployment.
 
-const publicVapidKey = 'BCG1GstLf8WYNGolMFC8OsIQdjuEPf_C_sDuDjr5Zg9WVS4iCMXOeWlYOeF3Qs_6KfJSY_robOjKQikY_fhfhfhfhhh87';
-const privateVapidKeys = 'JRc4xu9RiiOGGGvZhhzyq36s1s6aaTZKkeTimgffhhfhghgfh7798';
+const publicVapidKey = 'BCG1GstLf8WYNGolMFC8OsIQdjuEPf_C_sDuDjr5Zg9WVS4iCMXOeWlYOeF3Qs_6KfJSY_robOjKQikY_MRiye0';
+const privateVapidKeys = 'JRc4xu9RiiOGGGvZhhzyq36s1s6aaTZKkeTimgDVfX4';
 
-webpush.setVapidDetails('mailto:aahhhhxxxx@gmail.com', publicVapidKey, privateVapidKeys)
+webpush.setVapidDetails('mailto:aadusarkodie1@gmail.com', publicVapidKey, privateVapidKeys)
 
 
 //Create Subscribe Route to service workeer. POST request
@@ -39,6 +46,6 @@ app.post('/subscribe', (req, res) => {
 
 
 //Run the app. 
-const port = 6050;
+const port = 6000;
 
 app.listen(port, () => console.log(`Server is running on ${port}`))
