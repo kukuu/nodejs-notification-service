0ae25cdb75600394b8a5e5010c1b80db1b6c6595
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-05-17 10:28:08 +0100

    Registering Service Worker
    1.  Created client.js
    2. Check for Service worker
    3. Handling asynchronous behaviour with async await
    4. Register Service Worker

---
+++ client/client.js
@@ -0,0 +1,22 @@
+
+const publicVapidKey = 'BCG1GstLf8WYNGolMFC8OsIQdjuEPf_C_sDuDjr5Zg9WVS4iCMXOeWlYOeF3Qs_6KfJSY_robOjKQikY_MRiye0';
+
+//Check for Service worker
+if('serviceWorker' in navigator){
+	send().catch(err => console.errror(err));
+
+}
+
+//Handling asynchronous behaviour elegantly with async await
+//Register Service Worker, Register Push and Send Push
+async function send(){
+
+	//Register Service Worker
+	console.log('Registering Service Worker .....')
+
+	const register = await navigator.serviceWorker.register('/worker.js', {
+		scope: '/'
+	});
+
+	console.log('Service Worker registered....')
+}
