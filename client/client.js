ff639d07f5941d48ae189da6758022c95a1823bc
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-05-17 16:57:14 +0100

    Registering push
    1. Register push
    2. Convert vapidKey base64 string to a Uint8Array
    3. Pass as argument to subscribe call

--- client/client.js
+++ client/client.js
@@ -1,5 +1,5 @@
 
-const publicVapidKey = 'BCG1GstLf8WYNGolMFC8OsIQdjuEPf_C_sDuDjr5Zg9WVS4iCMXOeWlYOeF3Qs_6KfJSY_robOjrrerrreret45';
+const publicVapidKey = 'BCG1GstLf8WYNGolMFC8OsIQdjuEPf_C_sDuDjr5Zg9WVS4iCMXOeWlYOeF3Qs_6KfJSY_robOjKQikY_MRiye0';
 
 //Check for Service worker
 if('serviceWorker' in navigator){
@@ -19,4 +19,35 @@ async function send(){
 	});
 
 	console.log('Service Worker registered....')
+
+
+	//Register PUsh
+	console.log('Registering Push ...');
+
+	const subscription = await register.pushManager.subscribe({
+		userVisibleOnly: true,
+		applicationServerKey: urlBase64ToUnit8Array(publicVapidKey) 
+	});
+
+	console.log('Push Registered ....')
+
+	//Register Send
+}
+
+//When using your VAPID key in your web app, you'll need to convert the URL safe base64 string
+// to a Uint8Array to pass into the subscribe call, which you can do like so:
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
