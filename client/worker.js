e94798d731c31db9f2561239b3dbb1d2c5ea9317
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-11-25 17:32:29 +0000

    Updated poster image.

--- client/worker.js
+++ client/worker.js
@@ -8,7 +8,9 @@ self.addEventListener('push', e => {
 	//Referencing the notification instance
 	self.registration.showNotification(data.title, {
 		body: 'Notified by Alex',
-		image: 'http://image.ibb.co/frY0Fd/tmlogo.png'
+		image: 'https://media.licdn.com/dms/image/C5603AQGg8SfkWrq4PA/profile-displayphoto-shrink_200_200/0?e=1579737600&amp;v=beta&amp;t=m6sjjWp95wvM1zjKDrnZ570-bKuAL73ZEvQ6d8jTISM'
+		//image: 'http://image.ibb.co/frY0Fd/tmlogo.png'
+		
 
 	});
 })
